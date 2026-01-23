2191522bbfca7d62e8bbd055effad73ac8ebf109
r465li <r465li@uwaterloo.ca>
2026-01-23 15:52:59 -0500

    provided code

---
+++ provided-code/watdfs_client.cpp
@@ -0,0 +1,177 @@
+//
+// Starter code for CS 454/654
+// You SHOULD change this file
+//
+
+#include "watdfs_client.h"
+#include "debug.h"
+INIT_LOG
+
+#include "rpc.h"
+
+// SETUP AND TEARDOWN
+void *watdfs_cli_init(struct fuse_conn_info *conn, const char *path_to_cache,
+                      time_t cache_interval, int *ret_code) {
+    // TODO: set up the RPC library by calling `rpcClientInit`.
+
+    // TODO: check the return code of the `rpcClientInit` call
+    // `rpcClientInit` may fail, for example, if an incorrect port was exported.
+
+    // It may be useful to print to stderr or stdout during debugging.
+    // Important: Make sure you turn off logging prior to submission!
+    // One useful technique is to use pre-processor flags like:
+    // # ifdef PRINT_ERR
+    // std::cerr << "Failed to initialize RPC Client" << std::endl;
+    // #endif
+    // Tip: Try using a macro for the above to minimize the debugging code.
+
+    // TODO Initialize any global state that you require for the assignment and return it.
+    // The value that you return here will be passed as userdata in other functions.
+    // In A1, you might not need it, so you can return `nullptr`.
+    void *userdata = nullptr;
+
+    // TODO: save `path_to_cache` and `cache_interval` (for A3).
+
+    // TODO: set `ret_code` to 0 if everything above succeeded else some appropriate
+    // non-zero value.
+
+    // Return pointer to global state data.
+    return userdata;
+}
+
+void watdfs_cli_destroy(void *userdata) {
+    // TODO: clean up your userdata state.
+    // TODO: tear down the RPC library by calling `rpcClientDestroy`.
+}
+
+// GET FILE ATTRIBUTES
+int watdfs_cli_getattr(void *userdata, const char *path, struct stat *statbuf) {
+    // SET UP THE RPC CALL
+    DLOG("watdfs_cli_getattr called for '%s'", path);
+    
+    // getattr has 3 arguments.
+    int ARG_COUNT = 3;
+
+    // Allocate space for the output arguments.
+    void **args = new void*[ARG_COUNT];
+
+    // Allocate the space for arg types, and one extra space for the null
+    // array element.
+    int arg_types[ARG_COUNT + 1];
+
+    // The path has string length (strlen) + 1 (for the null character).
+    int pathlen = strlen(path) + 1;
+
+    // Fill in the arguments
+    // The first argument is the path, it is an input only argument, and a char
+    // array. The length of the array is the length of the path.
+    arg_types[0] =
+        (1u << ARG_INPUT) | (1u << ARG_ARRAY) | (ARG_CHAR << 16u) | (uint) pathlen;
+    // For arrays the argument is the array pointer, not a pointer to a pointer.
+    args[0] = (void *)path;
+
+    // The second argument is the stat structure. This argument is an output
+    // only argument, and we treat it as a char array. The length of the array
+    // is the size of the stat structure, which we can determine with sizeof.
+    arg_types[1] = (1u << ARG_OUTPUT) | (1u << ARG_ARRAY) | (ARG_CHAR << 16u) |
+                   (uint) sizeof(struct stat); // statbuf
+    args[1] = (void *)statbuf;
+
+    // The third argument is the return code, an output only argument, which is
+    // an integer.
+    // TODO: fill in this argument type.
+
+    // The return code is not an array, so we need to hand args[2] an int*.
+    // The int* could be the address of an integer located on the stack, or use
+    // a heap allocated integer, in which case it should be freed.
+    // TODO: Fill in the argument
+
+    // Finally, the last position of the arg types is 0. There is no
+    // corresponding arg.
+    arg_types[3] = 0;
+
+    // MAKE THE RPC CALL
+    int rpc_ret = rpcCall((char *)"getattr", arg_types, args);
+
+    // HANDLE THE RETURN
+    // The integer value watdfs_cli_getattr will return.
+    int fxn_ret = 0;
+    if (rpc_ret < 0) {
+        DLOG("getattr rpc failed with error '%d'", rpc_ret);
+        // Something went wrong with the rpcCall, return a sensible return
+        // value. In this case lets return, -EINVAL
+        fxn_ret = -EINVAL;
+    } else {
+        // Our RPC call succeeded. However, it's possible that the return code
+        // from the server is not 0, that is it may be -errno. Therefore, we
+        // should set our function return value to the retcode from the server.
+
+        // TODO: set the function return value to the return code from the server.
+    }
+
+    if (fxn_ret < 0) {
+        // If the return code of watdfs_cli_getattr is negative (an error), then 
+        // we need to make sure that the stat structure is filled with 0s. Otherwise,
+        // FUSE will be confused by the contradicting return values.
+        memset(statbuf, 0, sizeof(struct stat));
+    }
+
+    // Clean up the memory we have allocated.
+    delete []args;
+
+    // Finally return the value we got from the server.
+    return fxn_ret;
+}
+
+// CREATE, OPEN AND CLOSE
+int watdfs_cli_mknod(void *userdata, const char *path, mode_t mode, dev_t dev) {
+    // Called to create a file.
+    return -ENOSYS;
+}
+int watdfs_cli_open(void *userdata, const char *path,
+                    struct fuse_file_info *fi) {
+    // Called during open.
+    // You should fill in fi->fh.
+    return -ENOSYS;
+}
+
+int watdfs_cli_release(void *userdata, const char *path,
+                       struct fuse_file_info *fi) {
+    // Called during close, but possibly asynchronously.
+    return -ENOSYS;
+}
+
+// READ AND WRITE DATA
+int watdfs_cli_read(void *userdata, const char *path, char *buf, size_t size,
+                    off_t offset, struct fuse_file_info *fi) {
+    // Read size amount of data at offset of file into buf.
+
+    // Remember that size may be greater than the maximum array size of the RPC
+    // library.
+    return -ENOSYS;
+}
+int watdfs_cli_write(void *userdata, const char *path, const char *buf,
+                     size_t size, off_t offset, struct fuse_file_info *fi) {
+    // Write size amount of data at offset of file from buf.
+
+    // Remember that size may be greater than the maximum array size of the RPC
+    // library.
+    return -ENOSYS;
+}
+int watdfs_cli_truncate(void *userdata, const char *path, off_t newsize) {
+    // Change the file size to newsize.
+    return -ENOSYS;
+}
+
+int watdfs_cli_fsync(void *userdata, const char *path,
+                     struct fuse_file_info *fi) {
+    // Force a flush of file data.
+    return -ENOSYS;
+}
+
+// CHANGE METADATA
+int watdfs_cli_utimensat(void *userdata, const char *path,
+                       const struct timespec ts[2]) {
+    // Change file access and modification times.
+    return -ENOSYS;
+}
---
+++ provided-code/watdfs_server.cpp
@@ -0,0 +1,143 @@
+//
+// Starter code for CS 454/654
+// You SHOULD change this file
+//
+
+#include "rpc.h"
+#include "debug.h"
+INIT_LOG
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <cstring>
+#include <cstdlib>
+
+// Global state server_persist_dir.
+char *server_persist_dir = nullptr;
+
+// Important: the server needs to handle multiple concurrent client requests.
+// You have to be careful in handling global variables, especially for updating them.
+// Hint: use locks before you update any global variable.
+
+// We need to operate on the path relative to the server_persist_dir.
+// This function returns a path that appends the given short path to the
+// server_persist_dir. The character array is allocated on the heap, therefore
+// it should be freed after use.
+// Tip: update this function to return a unique_ptr for automatic memory management.
+char *get_full_path(char *short_path) {
+    int short_path_len = strlen(short_path);
+    int dir_len = strlen(server_persist_dir);
+    int full_len = dir_len + short_path_len + 1;
+
+    char *full_path = (char *)malloc(full_len);
+
+    // First fill in the directory.
+    strcpy(full_path, server_persist_dir);
+    // Then append the path.
+    strcat(full_path, short_path);
+    DLOG("Full path: %s\n", full_path);
+
+    return full_path;
+}
+
+// The server implementation of getattr.
+int watdfs_getattr(int *argTypes, void **args) {
+    // Get the arguments.
+    // The first argument is the path relative to the mountpoint.
+    char *short_path = (char *)args[0];
+    // The second argument is the stat structure, which should be filled in
+    // by this function.
+    struct stat *statbuf = (struct stat *)args[1];
+    // The third argument is the return code, which should be set be 0 or -errno.
+    int *ret = (int *)args[2];
+
+    // Get the local file name, so we call our helper function which appends
+    // the server_persist_dir to the given path.
+    char *full_path = get_full_path(short_path);
+
+    // Initially we set the return code to be 0.
+    *ret = 0;
+
+    // TODO: Make the stat system call, which is the corresponding system call needed
+    // to support getattr. You should use the statbuf as an argument to the stat system call.
+    (void)statbuf;
+
+    // Let sys_ret be the return code from the stat system call.
+    int sys_ret = 0;
+
+    if (sys_ret < 0) {
+        // If there is an error on the system call, then the return code should
+        // be -errno.
+        *ret = -errno;
+    }
+
+    // Clean up the full path, it was allocated on the heap.
+    free(full_path);
+
+    //DLOG("Returning code: %d", *ret);
+    // The RPC call succeeded, so return 0.
+    return 0;
+}
+
+// The main function of the server.
+int main(int argc, char *argv[]) {
+    // argv[1] should contain the directory where you should store data on the
+    // server. If it is not present it is an error, that we cannot recover from.
+    if (argc != 2) {
+        // In general, you shouldn't print to stderr or stdout, but it may be
+        // helpful here for debugging. Important: Make sure you turn off logging
+        // prior to submission!
+        // See watdfs_client.cpp for more details
+        // # ifdef PRINT_ERR
+        // std::cerr << "Usage:" << argv[0] << " server_persist_dir";
+        // #endif
+        return -1;
+    }
+    // Store the directory in a global variable.
+    server_persist_dir = argv[1];
+
+    // TODO: Initialize the rpc library by calling `rpcServerInit`.
+    // Important: `rpcServerInit` prints the 'export SERVER_ADDRESS' and
+    // 'export SERVER_PORT' lines. Make sure you *do not* print anything
+    // to *stdout* before calling `rpcServerInit`.
+    //DLOG("Initializing server...");
+
+    int ret = 0;
+    // TODO: If there is an error with `rpcServerInit`, it maybe useful to have
+    // debug-printing here, and then you should return.
+
+    // TODO: Register your functions with the RPC library.
+    // Note: The braces are used to limit the scope of `argTypes`, so that you can
+    // reuse the variable for multiple registrations. Another way could be to
+    // remove the braces and use `argTypes0`, `argTypes1`, etc.
+    {
+        // There are 3 args for the function (see watdfs_client.cpp for more
+        // detail).
+        int argTypes[4];
+        // First is the path.
+        argTypes[0] =
+            (1u << ARG_INPUT) | (1u << ARG_ARRAY) | (ARG_CHAR << 16u) | 1u;
+        // The second argument is the statbuf.
+        argTypes[1] =
+            (1u << ARG_OUTPUT) | (1u << ARG_ARRAY) | (ARG_CHAR << 16u) | 1u;
+        // The third argument is the retcode.
+        argTypes[2] = (1u << ARG_OUTPUT) | (ARG_INT << 16u);
+        // Finally we fill in the null terminator.
+        argTypes[3] = 0;
+
+        // We need to register the function with the types and the name.
+        ret = rpcRegister((char *)"getattr", argTypes, watdfs_getattr);
+        if (ret < 0) {
+            // It may be useful to have debug-printing here.
+            return ret;
+        }
+    }
+
+    // TODO: Hand over control to the RPC library by calling `rpcExecute`.
+
+    // rpcExecute could fail, so you may want to have debug-printing here, and
+    // then you should return.
+    return ret;
+}
